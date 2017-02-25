fdcc3b5a3f8b6045dcc54d7866c3c75eb5946366
Elisabeth <elisabeth.a.e.morgan@gmail.com>
2017-02-25 18:50:52 +0000

    25/2/17

--- Development/stattrial3.1.py
+++ Development/stattrial3.1.py
@@ -34,7 +34,7 @@ def get_probability(variable, dictionary):
 
     return probability, persistance
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('test_logs2.db')
 c = conn.cursor()
 
 programs = []
